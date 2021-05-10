59c1762d14422ee83e8da6e9c3f61ccff0f727f7
N <cubingniles@gmail.com>
2021-05-10 23:48:50 +0530

    Add event listener to check the key pressed

--- app.js
+++ app.js
@@ -33,4 +33,29 @@ function displayWord() {
     }
 }
 
+// Keydown letter press
+window.addEventListener('keydown', e => {
+    if(e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+
+        if(selectedWord.includes(letter)) {
+            if(!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord();
+            } else {
+                showNotifications();
+            }
+        } else {
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl();
+            } else {
+                showNotifications();
+            }
+        }
+    }
+});
+
 displayWord();
